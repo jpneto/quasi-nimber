ca23b1bcbe62930510cb1330f3943b03d09076df
João Pedro Neto <jpneto@fc.ul.pt>
2021-03-31 08:24:43 +0100

    Update quasi_nimber.py

--- quasi_nimber.py
+++ quasi_nimber.py
@@ -46,7 +46,7 @@ def unsafe(xs):
   """ return the indexes were xs[i] is false """
   return { i for i,x in enumerate(xs) if not x }
  
-   
+  
 def analyse(m, Ls, Rs):
   """ given table m with unsafe moves, eval position """
   n = len(Ls)
@@ -54,6 +54,14 @@ def analyse(m, Ls, Rs):
   # starting from nimber *pow2 the first player wins,
   # we only need to check unsafe moves for smaller nimber values  
   left_opts, right_opts = [False]*pow2, [False]*pow2
+  
+  max_Ls, max_Rs = max(map(len, Ls)), max(map(len, Rs))
+  if max_Ls > max_Rs:
+    left_opts = [True]*pow2
+  elif max_Ls < max_Rs:
+    right_opts = [True]*pow2 
+  else:
+    return (set(), set())
   
   for y in range(pow2):   # for each nimber upto pow2-1
   
@@ -96,21 +104,6 @@ def analyse(m, Ls, Rs):
           y_safe = y_safe or k_wins
       right_opts[y] = y_safe    
     
-    # now check if a shortcut exists
-    if not left_opts[y] and right_opts[y]:
-      # y is unsafe for Left but safe for Right
-      # Right wins the remaining y values: if he's playing, just move to y
-      for a in range(y+1,pow2):
-        right_opts[a] = True
-    elif left_opts[y] and not right_opts[y]:
-      # symmetric position
-      for a in range(y+1,pow2):
-        left_opts[a] = True
-    elif not left_opts[y] and not right_opts[y]:
-      # y is unsafe for both players, so all the next options are safe
-      for a in range(y+1,pow2):
-        left_opts[a] = right_opts[a] = True
-  
   # return the pair of unsafe values, ie, (LU(L,R), LU(R,L))
   return (unsafe(left_opts), unsafe(right_opts))
   
@@ -136,3 +129,74 @@ def solve(Ls, Rs):
       m[i][j] = solve(new_Ls, new_Rs)
 
   return analyse(m, Ls, Rs)
+
+##################################################
+# previous version
+
+# def analyse(m, Ls, Rs):
+#   """ given table m with unsafe moves, eval position """
+#   n = len(Ls)
+#   pow2 = next_pow2(m) 
+#   # starting from nimber *pow2 the first player wins,
+#   # we only need to check unsafe moves for smaller nimber values  
+#   left_opts, right_opts = [False]*pow2, [False]*pow2
+  
+#   for y in range(pow2):   # for each nimber upto pow2-1
+  
+#     if not left_opts[y]: # if *y not found already safe, check if it is
+#       # check left unsafe moves
+#       y_safe = False
+#       for i in range(n):    # for each row 
+#         if y_safe:
+#           break
+#         for k in Ls[i]:       # for each left move
+#           k_wins = True
+#           for j in range(n):    # for each column 
+#             if not k_wins:
+#               break
+#             for k1 in Rs[j]:      # for each right move
+#               # print('---',Ls[i],k,Rs[j],k1)
+#               if k^k1^y in m[i][j][0]:  # k is a losing move
+#                 k_wins = False
+#                 # print("y=",y,k,k1, m[i][j][0])
+#                 break
+#           # if k wins all columns, we found a winning move
+#           y_safe = y_safe or k_wins # only unsafe if all k's are losing moves
+#       left_opts[y] = y_safe
+    
+#     if not right_opts[y]: # if not found already safe, check if it is
+#       # check right unsafe moves
+#       y_safe = False
+#       for j in range(n):    # for each column
+#         if y_safe:
+#           break
+#         for k in Rs[j]:       # for each right move
+#           k_wins = True
+#           for i in range(n):    # for each row
+#             if not k_wins:
+#               break
+#             for k1 in Ls[i]:      # for each left move
+#               if k^k1^y in m[i][j][1]:  # k is a losing move
+#                 k_wins = False
+#                 break
+#           y_safe = y_safe or k_wins
+#       right_opts[y] = y_safe    
+    
+#     # now check if a shortcut exists
+#     if not left_opts[y] and right_opts[y]:
+#       # y is unsafe for Left but safe for Right
+#       # Right wins the remaining y values: if he's playing, just move to y
+#       for a in range(y+1,pow2):
+#         right_opts[a] = True
+#     elif left_opts[y] and not right_opts[y]:
+#       # symmetric position
+#       for a in range(y+1,pow2):
+#         left_opts[a] = True
+#     elif not left_opts[y] and not right_opts[y]:
+#       # y is unsafe for both players, so all the next options are safe
+#       for a in range(y+1,pow2):
+#         left_opts[a] = right_opts[a] = True
+  
+#   # return the pair of unsafe values, ie, (LU(L,R), LU(R,L))
+#   return (unsafe(left_opts), unsafe(right_opts))
+  
